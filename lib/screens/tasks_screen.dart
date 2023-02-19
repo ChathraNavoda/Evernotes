a60531935c72ac9edd38ec33f78c4726cdd34943
Chathra Navoda Ranasinghe <91416868+ChathraNavoda@users.noreply.github.com>
2023-02-19 15:21:29 +0530

    add task

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,12 +1,33 @@
 import 'package:evernotes/constants/global_variables.dart';
 import 'package:evernotes/model/task.dart';
+import 'package:evernotes/screens/add_task_sceen.dart';
 import 'package:evernotes/widgets/tasks_list.dart';
 import 'package:flutter/material.dart';
 import '../blocs/bloc_exports.dart';
 import '../widgets/tasks_list.dart';
 
-class TasksScreen extends StatelessWidget {
+class TasksScreen extends StatefulWidget {
   const TasksScreen({Key? key}) : super(key: key);
+
+  @override
+  State<TasksScreen> createState() => _TasksScreenState();
+}
+
+class _TasksScreenState extends State<TasksScreen> {
+  TextEditingController titleController = TextEditingController();
+
+  void _addTask(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) => SingleChildScrollView(
+        child: Container(
+          padding:
+              EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+          child: const AddTaskScreen(),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +39,7 @@ class TasksScreen extends StatelessWidget {
             title: const Text('✏️ Evernotes'),
             actions: [
               IconButton(
-                onPressed: () {},
+                onPressed: () => _addTask(context),
                 icon: const Icon(Icons.add),
               )
             ],
@@ -38,7 +59,7 @@ class TasksScreen extends StatelessWidget {
             ],
           ),
           floatingActionButton: FloatingActionButton(
-            onPressed: () {},
+            onPressed: () => _addTask(context),
             tooltip: 'Add Task',
             child: const Icon(Icons.add),
           ),
